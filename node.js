c691519d56b269c4904e1d83f9348d2400bf9f35
Vladimir Dronnikov <dronnikov@gmail.com>
2012-04-01 20:22:34 +0400

    node test client updated for pipeline

--- node.js
+++ node.js
@@ -2,10 +2,40 @@
 
 var body = 'Hello\n';
 require('http').createServer(function (req, res) {
-  req.on('end', function() {
-    res.writeHead(200, {
-      'Content-Length': body.length,
-    });
-    res.end(body);
+  req.on('end', function () {
+    if (req.url === '/1') {
+      setTimeout(function () {
+      res.writeHead(200, {
+        'Content-Length': body.length,
+      });
+      res.end('[111]\n');
+      }, 20);
+    } else if (req.url === '/2') {
+      setTimeout(function () {
+      res.writeHead(200, {
+        'Content-Length': body.length,
+      });
+      res.end('[222]\n');
+      }, 0);
+    } else if (req.url === '/3') {
+      setTimeout(function () {
+      res.writeHead(200, {
+        'Content-Length': body.length,
+      });
+      res.end('[333]\n');
+      }, 30);
+    } else if (req.url === '/4') {
+      setTimeout(function () {
+      res.writeHead(200, {
+        'Content-Length': body.length,
+      });
+      res.end('[444]\n');
+      }, 10);
+    } else {
+      res.writeHead(200, {
+        'Content-Length': body.length,
+      });
+      res.end(body);
+    }
   });
 }).listen(8080);
